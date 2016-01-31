ba469a0b7b29968afee293463ed8ee30a44c2c95
Itay Grudev <itay@grudev.com>
2016-01-31 04:57:41 +0000

    Added Elastic Search support to model

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,55 @@
 class Page < ActiveRecord::Base
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
+  index_name Rails.application.class.parent_name.underscore
+  document_type 'page'
+
+  settings index: { number_of_shards: 1 } do
+    mapping dynamic: false do
+      indexes :title, analyzer: 'english'
+      indexes :body, analyzer: 'english'
+    end
+  end
+
+  def self.search(query)
+    __elasticsearch__.search(
+      {
+        query: {
+          multi_match: {
+            query: query,
+            fields: ['title^5', 'body']
+          }
+        },
+        highlight: {
+          pre_tags: ['<mark>'],
+          post_tags: ['</mark>'],
+          fields: {
+            title: {},
+            body: {},
+          }
+        },
+        suggest: {
+          text: query,
+          title: {
+            term: {
+              size: 1,
+              field: :title
+            }
+          },
+          body: {
+            term: {
+              size: 1,
+              field: :body
+            }
+          }
+        }
+      }
+    )
+  end
+
+  def as_indexed_json(options = nil)
+    self.as_json( only: [ :title, :body ] )
+  end
+
 end

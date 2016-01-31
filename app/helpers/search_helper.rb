700a47c1b5b1de256ec315f9343f1125990f71ac
Itay Grudev <itay@grudev.com>
2016-01-31 04:58:26 +0000

    Implemented search controller

---
+++ app/helpers/search_helper.rb
@@ -0,0 +1,44 @@
+module SearchHelper
+
+  def autosuggest_aggregate(response, query)
+    # Stores unique words and their autocorrect suggestions
+    words = { }
+
+    # Itterate over fields
+    [:title, :body, :tags].each do |field|
+      # Itterate over query words
+      response.send(field).to_a.each do |word|
+        # If any options are available
+        if word.options.length > 0
+          # Append word if it doesn't already exist
+          new_word = false
+          unless words.include? word.text
+            words[word.text] = { text: '', score: nil, freq: nil }
+            new_word = true
+          end
+          word.options.each do |option|
+            if new_word or words[word.text][:score] * words[word.text][:freq] < option[:score] * option[:freq]
+              words[word.text][:text] = option[:text]
+              words[word.text][:score] = option[:score]
+              words[word.text][:freq] = option[:freq]
+            end
+          end
+        end
+      end
+    end
+
+    new_query = query.dup
+
+    # Generate a new string based on the high score suggestions
+    words.each do |word, suggestion|
+      if suggestion[:score] > 0.65
+        new_query[word] = suggestion[:text]
+      end
+    end
+
+    # Return the new query or false if there were no modifications
+    return new_query unless new_query == query
+    false
+  end
+
+end
